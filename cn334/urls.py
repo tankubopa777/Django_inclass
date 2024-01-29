20231cfd3134035d612e9cd34fa96deaee516212
ワルンポ-ンさん <tankubopa77@gmail.com>
2024-01-29 23:12:42 +0700

    update

--- cn334/urls.py
+++ cn334/urls.py
@@ -21,5 +21,10 @@ from ecommerce import views as ecom_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("ecommerce/", ecom_views.ecommerce_index_view),
-    path("ecommerce/item/<item_id>", ecom_views.item_view)
+    path("ecommerce/item/<item_id>", ecom_views.item_view),
+    path("ecommerce/homepage", ecom_views.homepage_view),
+    path("ecommerce/catagory", ecom_views.catagory_view),
+    path("ecommerce/product", ecom_views.product_view),
+    path("ecommerce/checkout", ecom_views.checkout_view),
+
 ]
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -11,4 +11,19 @@ def item_view(request, item_id):
        "item_id": item_id
 }
    return render(request, 'index.html',context = context_data)
+
+def homepage_view(request):
+    return render(request, 'homepage.html')
+
+def catagory_view(request):
+    return render(request, 'catagory.html')
+
+def product_view(request):
+    return render(request, 'product.html')
+
+def checkout_view(request):
+    return render(request, 'checkout.html')
+
+def contact_view(request):
+    return render(request, 'contact.html')
     
